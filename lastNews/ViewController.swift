b3eaae1e6efadf97bed3c00658655db2df7cc151
Maksim Pavlovich <max.pav.love@icloud.com>
2021-11-16 17:19:09 +0300

    Initial Commit

---
+++ lastNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lastNews
+//
+//  Created by Максим Хлесткин on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
